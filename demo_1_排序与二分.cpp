d1347435cc3eb04ae12824649a9c31fe0425a13e
bruceweng <bruceweng@tencent.com>
2021-07-30 16:41:10 +0800

    增加文件

---
+++ demo_1_排序与二分.cpp
@@ -0,0 +1,87 @@
+//快速排序模板--基于分治
+void quick_sort(int a[], int l, int r)
+{
+	if (l >= r)	return;
+	
+	int x = a[l], i = l - 1, j = r + 1;
+	
+	while (i < j)
+	{
+		do ++i;	while(a[i] < x);
+		do --j; while(a[j] > x);
+		if (i < j)	swap(a[i], a[j]);
+	}
+	
+	quick_sort(a, l, j);
+	quick_sort(a, j + 1, r);
+}
+void quick_sort(int a[], int l; int r)
+{
+	if (l >= r)	return;
+	
+	int x = a[l], i = l - 1, j = r + 1;
+	while (i < j)
+	{
+		do ++i; while(a[i] < x);
+		do --j; while(a[j] > x);
+		if (i < j)	swap(a[i], a[j]);
+	}
+	
+	quick_sort(a, l, j);
+	quick_sort(a, j + 1, r);
+}
+void quick_sort(int a[], int l, int r)
+{
+	if (l >= r)	return;
+	
+	int x = a[l], i = l - 1, j = r + 1;
+	
+	while (i < j)
+	{
+		do ++i; while(a[i] < x);
+		do --j; while(a[j] > x);
+		if (i < j)	swap(a[i], a[j]);
+	}
+	
+	quick_sort(a, l, j);
+	quick_sort(a, j + 1, r);
+}
+void quick_sort(int a[], int l, int r)
+{
+	if (l >= r)	return;
+	int x = a[l], i = l - 1, j = r + 1;
+	
+	while (i < j)
+	{
+		do ++i; while(a[i] < x);
+		do --j; while(a[j] > x);
+		if (i < j)	swap(a[i], a[j]);
+	}
+	
+	quick_sort(a, l, j);
+	quick_sort(a, j + 1, r);
+}
+
+//归并排序--基于分治
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
