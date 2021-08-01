2de70df98b6231b90fa2feb879c68a948467d150
amd <amd>
2021-08-01 12:42:20 +0800

    文件更新

--- demo_2_高精度-前缀和-差分.cpp
+++ demo_2_高精度-前缀和-差分.cpp
@@ -213,11 +213,194 @@ vector<int> sub(vector<int> &a, vector<int> &b)
 	return c;
 }
 
+//高精度乘法，大正数乘一个普通的正数，注意这里使用大数的每一位乘普通的数
+vector<int> mul(vector<int> &a, int b)
+{
+	vector<int> c;
+	int t = 0;	//表示进位
+	
+	while (int i = 0; i < a.size() || t; ++i)
+	{
+		if (i < a.size())	t += a[i] * b;
+		c.push_back(t % 10);
+		t /= 10;
+	}
+	
+	return c;
+}
+vector<int> mul(vector<int> &a, vector<int> &b)
+{
+	vector<int> c;
+	int t = 0;
+	
+	for (int i = 0; i < a.size() || t; ++i)
+	{
+		if (i < a.size())	t += a[i] * b;
+		c.push_back(t % 10);
+		t /= 10;
+	}
+	
+	return t;
+}
+vector<int> mul(vector<int> &a, vector<int> &b)
+{
+	vector<int> c;
+	int t = 0;
+	
+	for(int i = 0; i < a.size() || t; ++i)
+	{
+		if (i < a.size())	t += a[i] * b;
+		c.push_back(t % 10);
+		t /= 10;
+	}
+	
+	return c;
+}
+vector<int> mul(vector<int> &a, int b)
+{
+	vector<int> c;
+	int t = 0;
+	
+	for (int i = 0; i < a.size() || t, ++i)
+	{
+		if (i < a.size())	t += a[i] * b;
+		c.push_back(t % 10);
+		t /= 10;
+	}
+	
+	return c;
+}
+vector<int> mul(vector<int> &a, int b)
+{
+	vector<int> c;
+	int t = 0;
+	
+	for (int i = 0; i < a.size() || t; ++i)
+	{
+		if (i < a.size())	t += a[i] * b;
+		c.push_back(t % 10);
+		t /= 10;
+	}
+	
+	return c;
+}
 
+//高精度除法，大正数除一个普通的正数，模拟除法
+vector<int> div(vector<int> &a, int b, int &r)	//a / b = c...r
+{
+	vector<int> c;
+	r = 0;	//r为余数
+	
+	for (int i = a.size() - 1; i >= 0; --i)
+	{
+		r = r * 10 + a[i];
+		c.push_back(r / b);
+		r %= b;
+	}
+	
+	reserve(c.begin(), c.end());
+	while (c.size() > 1 && c.back() == 0)	c.pop_back();
+	
+	return c;
+}
+vector<int> div(vector<int> &a, int b, int &r)
+{
+	vector<int> c;
+	r = 0;
+	
+	for (int i = a.size() - 1; i >= 0; --i)
+	{
+		r = r * 10 + a[i];
+		c.push_back(r / b);
+		r %= b;
+	}
+	
+	reserve(c.begin(), c.end());
+	while (c.size() > 1 && c.back() == 0)	c.pop_back();
+	
+	return c;
+}
+vector<int> div(vector<int> &a, int b, int &r)
+{
+	vector<int> c;
+	r = 0;
+	
+	for (int i = a.size() - 1; i >= 0; --i)
+	{
+		r = r * 10 + a[i];
+		c.push_back(r / 10);
+		r %= 10;
+	}
+	
+	reserve(c.begin(), c.end());
+	while (c.size() > 1 && c.back() == 0)	c.pop_back();
+	
+	return c;
+}
+vector<int> div(vector<int> &a, int b, int &r)
+{
+	vector<int> c;
+	r = 0;
+	
+	for (int i = a.size() - 1, i >= 0; --i)
+	{
+		r = 10 * r + a[i];
+		c.push_back(r / b);
+		r %= b;
+	}
+	
+	c.reservve(c.begin(), c.end());
+	while (c.size() > 1 && c.back() == 0)	c.pop_back();
+	
+	return c;
+}
 
+//一维前缀和
+const int N = 100010
+int a[N], s[N];
+for (int i = 1; i <= n; ++i)
+{
+	s[i] = s[i - 1] + a[i];
+}
+for (int i = 1; i <= n; ++i)
+{
+	a[i] = a[i] + a[i - 1];		//自己变成自己的前缀和
+}
+for (int i = 1; i <= n; ++i)
+{
+	s[i] = s[i - 1] + a[i];
+}
+for (int i = 1; i <= n; ++i)
+{
+	s[i] = s[i - 1] + a[i];
+}
+for (int i = 1; i <= n; ++i)
+{
+	s[i] = s[i - 1] + a[i];
+}
+for (int i = 1; i <= n; ++i)
+{
+	s[i] = s[i - 1] + a[i];
+}
+for (int i = 1; i <= n; ++i)
+{
+	s[i] = s[i - 1] + a[i];
+}
+//二维前缀和
+const int N = 100010;
+int a[N][N], b[N][N];
+for (int i = 1; i <= n; ++i)
+{
+	for (int j = 1; j <= m; ++j)
+	{
+		s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + a[i][j];
+	}
+}
+求左上角为x1y1右下角为x2y2的矩形的所有数的和
+sum = s[x2][y2] - s[x2][y1 - 1] - s[x1 - 1][y2] + s[x1 - 1][y1 - 1];
 
-
-
+//一维差分，原数组是差分数组的前缀和，原数组从a[l]到a[r]每个数加c只要差分数组b[l] = b[l] + c, b[r + 1] = b[r + 1] - c之后求前缀和即可得到所求的a
+//最开时a是已经给出的，b是全0的，可以看成a从全0开始[1, 1]加a[1]，[2, 2]加a[2]，...，[n, n]加a[n]
 
 
 
