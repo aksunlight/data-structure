a1ce6925053d83609ada5a0dd2c29cd2800721d4
bruceweng <bruceweng@tencent.com>
2021-08-02 10:21:23 +0800

    文件更新

--- demo_2_高精度-前缀和-差分.cpp
+++ demo_2_高精度-前缀和-差分.cpp
@@ -503,6 +503,20 @@ for (int i = 1; i <= n; ++i)
 		b[i][j] = b[i - 1][j] + b[i][j - 1] - b[i - 1][j - 1] + b[i][j];
 	}
 }
+void insert(int** b, int x1, int y1, int x2, int y2, int c)
+{
+	b[x1][y1] += c;
+	b[x1][y2 + 1] -= c;
+	b[x2 + 1][y1] -= c;
+	b[x2 + 1][y2 + 1] += c;
+}
+for (int i = 1; i <= n; ++i)
+{
+	for (int j = 1; j <= m; ++j)
+	{
+		b[i][j] = b[i - 1][j] + b[i][j - 1] - b[i - 1][j - 1] + b[i][j];
+	}
+}
 
 
 
