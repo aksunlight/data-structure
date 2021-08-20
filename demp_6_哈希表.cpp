f1ca222848fd4ae34fc39af507d062da63b0fe75
bruceweng <bruceweng@tencent.com>
2021-08-20 10:29:03 +0800

    文件修改

--- demp_6_哈希表.cpp
+++ demp_6_哈希表.cpp
@@ -24,8 +24,85 @@ bool find(int x)
 	}
 	return false;
 }
-
-
+const int N = 100010;
+int h[N], e[N], ne[N], idx;
+memset(h, -1, sizeof(h));
+void insert(int x)
+{
+	int k = (x % N + N) % N;
+	e[idx] = x;
+	ne[idx] = h[k];
+	h[k] = idx++;
+}
+bool find(int x)
+{
+	int k = (x % N + N) % N;
+	for (int i = h[k]; i != -1; i = ne[i])
+	{
+		if (e[i] == x)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+const int N = 100010;
+int h[N], e[N], ne[N], idx;
+memset(h, -1, memset(h));
+void insert(int x)
+{
+	int k = (x % N + N) % N;
+	e[idx] = x;
+	ne[idx] = h[k];
+	h[k] = idx++;
+}
+bool find(int x)
+{
+	int k = (x % N + N) % N;
+	for (int i = h[k];; i != -1; i = ne[i])
+	{
+		if (x == e[i])	return true;
+	}
+	return false;
+}
+const int N = 100010;
+int h[N], e[N], ne[N], idx;
+memset(h, -1, sizeof(h));
+void insert(int x)
+{
+	int k = (x % N + N) % N;
+	e[idx] = x;
+	ne[idx] = h[k];
+	h[k] = idx++;
+}
+bool find(int x)
+{
+	int k = (x % N + N) % N
+	for (int i = h[k]; i != -1; i = ne[i])
+	{
+		if (x == e[i])	return true;
+	}
+	return false;
+}
+const int N = 100010;
+int h[N], e[N], ne[N], idx;
+memset(h, -1, sizeof(h));
+void insert(int x)
+{
+	int k = (x % N + N) % N;
+	e[idx] = x;
+	ne[idx] = h[k];
+	h[k] = idx++;
+}
+bool find(int x)
+{
+	int k = (x % N + N) % N;
+	for (int i = h[k]; i != -1; i = ne[i])
+	{
+		if (x == e[i])	return true;
+	}
+	return false;
+}
 
 
 
