ad45c28bfb10575f97400a6517f233844bd7e75c
marst <marst>
2022-06-09 14:04:29 +0800

    修改文件

--- demo_1_排序与二分.cpp
+++ demo_1_排序与二分.cpp
@@ -1,7 +1,10 @@
-//快速排序模板--基于分治
+//快速排序模板--基于分治 O(nlogn)
+//1.确定分界点：q[l], q[(l+r)/2], q[r], 随机
+//2.调整区间：左边区间里的数都小于等于分界点的值，右边区间的数都大于等于分界点的值
+//3.递归处理左右两段
 void quick_sort(int q[], int l, int r)
 {
-    if (l >= r) return;
+    if (l >= r) return; //if (l == r)	return;
 
     int i = l - 1, j = r + 1, x = q[l + r >> 1];
     while (i < j)
@@ -14,8 +17,40 @@ void quick_sort(int q[], int l, int r)
 }
 void quick_sort(int a[], int l, int r)
 {
+	if (l >= r)	return;
+
+	int x = a[l], i = l - 1, j = r + 1;
+	while (i < j)
+	{
+		do ++i; while (a[i] < x);
+		do --j; while (a[j] > x);
+		if (i < j)	swap(a[i], a[j]);
+	}
+	quick_sort(a, l, j), quick_sort(a, j + 1, r);
+}
+void quick_sort(int a[], int l, int r)
+{
+	if (l >= r)	return;
 	int x = a[l + r >> 1], i = l - 1, j = r + 1;
-	
+	while (i < j)
+	{
+		do ++i; while (a[i] < x);
+		do --j; while (a[j] > x);
+		if (i < j)	swap(a[i], a[j]);
+	}
+	quick_sort(a, l, j), quick_sort(a, j + 1, r);
+}
+void quick_sort(int a[], int l, int r)
+{
+	if (l >= r)	return;
+	int x = a[l + r >> 1], i = l - 1, j = r + 1;
+	while (i < j)
+	{
+		do ++i;	while (a[i] < x);
+		do --j; while (a[j] > x);
+		if (i < j)	swap(a[i], a[j]);
+	}
+	quick_sort(a, l, j), quick_sort(a, j + 1, r);
 }
 void quick_sort(int a[], int l, int r)
 {
@@ -175,7 +210,10 @@ void quick_sort(int a[], int l, int r)
 	quick_sort(a, j + 1, r);
 }
 
-//归并排序--基于分治
+//归并排序--基于分治 nlogn
+//1.确定分界点mid = l + r >> 2
+//2.递归排序对半的左右两边
+//3.拿左右两边第一个数两两比较进行归并；合二为一
 void merge_sort(int q[], int l, int r)
 {
     if (l >= r) return;
@@ -194,10 +232,34 @@ void merge_sort(int q[], int l, int r)
 
     for (i = l, j = 0; i <= r; i ++, j ++ ) q[i] = tmp[j];
 }
-作者：yxc
-链接：https://www.acwing.com/blog/content/277/
-来源：AcWing
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+void merge_sort(int a[], int l, int r)
+{
+	if (l >= r)	return;
+	int mid = l + r >> 1;
+	merge_sort(a, l, mid), merge_sort(a, mid + 1, r);
+	int k = 0, i = l, j = mid + 1;
+	while (i <= mid && j <= r)
+		if (a[i] <= a[j])	tmp[k++] = a[i++];
+		else	tmp[k++] = a[j++];
+	while (i <= mid)	tmp[k++] = a[i++];
+	while (j <= r)	tmp[k++] = a[j++];
+	for (i = l, j = 0; i <= r; ++i, ++j)	a[i] = tmp[j];
+}
+void merge_sort(int a[], int l, int r)
+{
+	if (l >= r)	return;
+	int mid = l + r >> 1;
+	merge_sort(a, l, mid), merge_sort(a, mid + 1, r);
+	int i = l, j = mid + 1, k = 0;
+	while (i <= mid && j <= r)
+	{
+		if (a[i] <= a[j])	b[k++] = a[i++];
+		else	b[k++] = a[i++];
+	}
+	while (i <= mid)	b[k++] = a[i++];
+	while (j <= r)	b[k++] = a[j++];
+	for (i = l, j = 0; i <= r; ++i, ++j)	a[i] = b[i];
+}
 void merge_sort(int a[], int l, int r)
 {
 	if (l >= r)	return;
@@ -322,14 +384,14 @@ void merge_sort(int a[], int l, int r)
 	for (int i = l, j = 0; i <= r; ++i, ++j)	a[i] = b[j];
 }
 
-//二分查找，找出左半段的右边界1和右半段的左边界2
+//二分查找，某个整数可以分为左右半段，找出左半段的右边界1和右半段的左边界2
 bool check(int x)	{...}	//检查x是否满足某种性质
 int bsearch_1(int l, int r)	//找出左半段的右边界
 {
 	while (l < r)
 	{
 		int mid = (l + r + 1) >> 1;
-		if (check(mid))	l = mid;
+		if (check(mid))	l = mid;	//判断mid在不在左半段
 		else	r = mid - 1;
 	}
 	
@@ -341,10 +403,30 @@ int bsearch_2(int l, int r)	//查找右半段的左边界
 	while (l < r)
 	{
 		int mid = (l + r) >> 1;
-		if (check(mid))	r = mid;
+		if (check(mid))	r = mid;	//判断mid在不在右半段
 		else l = mid + 1;
 	}
 	
+	return l;
+}
+int bsearch_1(int l, int r)
+{
+	while (l < r)
+	{
+		int mid = (l + r + 1) >> 1;
+		if (check(mid))	l = mid;
+		else	r = mid - 1;
+	}
+	return l;
+}
+int bsearch_2(int l, int r)
+{
+	while (l < r)
+	{
+		int mid = (l + r) >> 1;
+		if (check(mid))	r = mid;
+		else	l = mid + 1;
+	}
 	return l;
 }
 bool check(double x) {...}	//检查x是否满足某种性质
@@ -359,75 +441,24 @@ double bsearch_3(double l, double r)
 	}
 	return l;
 }
-bool check(int x)	{...}
-int bsearch_1(int l, int r)
+//开平方
+#include <iostream>
+int main()
 {
-	while (l < r)
-	{
-		int mid = (l + r) >> 1;
-		if (check(mid))	r = mid;
-		else	l = mid + 1;
-	}
-	
-	return l;
-}
-int bsearch_2(int l, int r)
-{
-	while (l < r)
-	{
-		int mid = (l + r + 1) >> 1;
-		if (check(mid))	l = mid;
-		else	r = mid - 1;
-	}
-	
-	return l;
-}
-double check(double x)	{...}
-double bsearch(double l, double r)
-{
-	const double eps = 1e-6;
-	while (r - l > eps)
-	{
-		double mid = (l + r) / 2;
-		if (check(mid))	r = mid;
-		else l = mid;
-	}
-	return l;
-}
-int check(int x)	{...}
-int bsearch_1(int l, int r)
-{
-	while (l < r)
-	{
-		int mid = (l + r) >> 1;
-		if (check(mid))	r = mid;
-		else l = mid + 1;
-	}
-	return l;
-}
-int bsearch_2(int l, int r)
-{
-	while (l < r)
-	{
-		int mid = (l + r + 1) >> 1;
-		if (check(mid))	l = mid;
-		else r = mid - 1;
-	}
-	return l;
-}
-double check(double x)	{...}
-double bsearch_3(double l, double r)
-{
-	const double eps = 1e-6;
-	while (r - l > eps)
-	{
-		double mid = (l + r) / 2;
-		if (check(mid))	r = mid;
-		else l = mid;
-	}
-	return l;
-}
+	double x;
+	cin >> x;
 
+	double l = 0, r = x;
+	while (r - l > 1e-8)    //直接迭代100次 for(int i = 1; i <= 100; ++i)
+	{
+		double mid = (l + r) / 2;
+		if (mid * mid >= x)	r = mid;
+		else	l = mid;
+	}
+	cout << l << endl;
+
+	return 0;
+}
 
 
 
--- demo_2_高精度-前缀和-差分.cpp
+++ demo_2_高精度-前缀和-差分.cpp
@@ -1,5 +1,68 @@
 //高精度加法
-vector<int> add(vector<int> &A, vector<int> &B)	//A和B数组存储大正数的逆序，返回的数组也是大正数的逆序
+// C = A + B, A >= 0, B >= 0
+vector<int> add(vector<int> &A, vector<int> &B)
+{
+    if (A.size() < B.size())	return add(B, A);
+
+    vector<int> C;
+    int t = 0;
+    for (int i = 0; i < A.size(); i ++ )
+    {
+        t += A[i];
+        if (i < B.size()) t += B[i];
+        C.push_back(t % 10);
+        t /= 10;
+    }
+
+    if (t) C.push_back(t);
+    return C;
+}
+vector<int> add(vector<int>& A, vector<int>& B)
+{
+	if (A.size() < B.size())	return add(B, A);
+	vector<int> C;
+	int t = 0;
+	for (int i = 0; i < A.size(); ++i)
+	{
+		t += A[i];
+		if (i < B.size())	t += B[i];
+		C.push_back(t % 10);
+		t /= 10;
+	}
+	if (t)	C.push_back(t);
+	return C;
+}
+vector<int> add(vector<int> &A, vector<int> &B)
+{
+	if (A.size() < B.size())	return add(B, A);
+	vector<int> C;
+	int t = 0;
+	for (int i = 0; i < A.size(); ++i)
+	{
+		t += A[i];
+		if (i < B.size())	t += B[i];
+		C.push_back(t % 10);
+		t /= 10;
+	}
+	if (t)	C.push_back(t);
+	return C;
+}
+vector<int> add(vector<int>& A, vector<int>& B)
+{
+	if (A.size() < B.size())	return add(B, A);
+	vector<int> C;
+	int t = 0;
+	for (int i = 0; i < A.size(); ++i)
+	{
+		t += A[i];
+		if (i < B.size())	t += B[i];
+		C.push_back(t % 10);
+		t /= 10;
+	}
+	if (t)	C.push_back(t);
+	return C;
+}
+vector<int> add(vector<int> &A, vector<int> &B)	//A和B数组存储大整数的逆序，返回的数组也是大整数的逆序
 {
 	vector<int> C;
 	int t = 0;	//t表示进位，初始为0
@@ -121,8 +184,97 @@ vector<int> add(vector<int> &a, vector<int> &b)
 	return c;
 }
 
-//高精度减法（都转为大的正数减小的正数, a的位数大于b或者位数相同）
-vector<int>	sub(vector<int> &a, vector<int> &b)	////A和B数组存储大正数的逆序，返回的数组也是大正数的逆序
+//高精度减法（都转为大的整数减小的整数, a的位数大于等于b）
+// C = A - B, 满足A >= B, A >= 0, B >= 0
+bool cmp(vector<int>& A, vector<int>& B)
+{
+	if (A.size() != B.size())	return A.size() > B.size();
+	for (int i = A.size() - 1; i >= 0; --i)
+	{
+		if (A[i] != B[i])
+		{
+			return A[i] > B[i];
+		}
+	}
+	return true;
+}
+vector<int> sub(vector<int> &A, vector<int> &B)
+{
+    vector<int> C;
+    for (int i = 0, t = 0; i < A.size(); i ++ )
+    {
+        t = A[i] - t;
+        if (i < B.size()) t -= B[i];
+        C.push_back((t + 10) % 10);
+        if (t < 0) t = 1;
+        else t = 0;
+    }
+
+    while (C.size() > 1 && C.back() == 0) C.pop_back();	//去掉前导零
+    return C;
+}
+int main()
+{
+	string a, b;
+	vector<int> A, B;
+	cin >> a >> b;
+	for (int i = a.size() - 1; i >= 0; --i)
+	{
+		A.push_back(a[i] - '0');
+	}
+	for (int i = b.size() - 1; i >= 0; --i)
+	{
+		B.push_back(b[i] - '0');
+	}
+	if (cmp(A, B))
+	{
+		auto C = sub(A, B);
+		for (int i = C.size() - 1; i >= 0; --i)
+		{
+			printf("%d", C[i]);
+		}
+	}
+	else
+	{
+		auto C = sub(B, A);
+		printf("-");
+		for (int i = C.size() - 1; i >= 0; --i)
+	    {
+			printf("%d", C[i]);
+		}
+	}
+
+	return 0;
+}
+vector<int> sub(vector<int>& a, vector<int>& b)
+{
+	vector<int> c;
+	int t = 0;
+	for (int i = 0; i < a.size(); ++i)
+	{
+		t = a[i] - t;
+		if (i < b.size())	t -= b[i];
+		c.push_back((t + 10) % 10);
+		if (t < 0)	t = 1;
+		else	t = 0;
+	}
+	while (c.size() > 1 && c.back() == 0)	c.pop_back();
+	return c;
+}
+vector<int> sub(vector<int>& a, vector<int>& b)
+{
+	vector<int> c;
+	int t = 0;
+	for (int i = 0; i < a.size(); ++i)
+	{
+		t = a[i] - t;
+		if (i < b.size())	t -= b[i];
+		if (t < 0)	t = 1;
+		else	t = 0;
+	}
+	while (b.size() > 1 && b.back() == 0)	c.pop_back();
+}
+vector<int>	sub(vector<int> &a, vector<int> &b)	////A和B数组存储大整数的逆序，返回的数组也是大整数的逆序
 {
 	vector<int> c;
 	int t = 0;	//借位
@@ -303,6 +455,20 @@ vector<int> div(vector<int> &a, int b, int &r)	//a / b = c...r
 	
 	return c;
 }
+vector<int> div(vector<int>& a, int b, int& r)
+{
+	vector<int> c;
+	r = 0;
+	for (int i = a.size() - 1; i >= 0; --i)
+	{
+		r = r * 10 + a[i];
+		c.push_back(r / b);
+		r = r % b;
+	}
+	reserve(c.begin(), c.end());
+	while (c.siez() > 1 && c.back() == 0)	c.pop_back();
+	return c;
+}
 vector<int> div(vector<int> &a, int b, int &r)
 {
 	vector<int> c;
@@ -386,7 +552,7 @@ for (int i = 1; i <= n; ++i)
 {
 	s[i] = s[i - 1] + a[i];
 }
-//二维前缀和，s[i][j]表示a[1-i][1-j]的所有数的和，即左上矩阵和
+//二维前缀和，s[i][j]表示a[1~i][1~j]的所有数的和，即左上矩阵和
 const int N = 100010;
 int a[N][N], s[N][N];
 for (int i = 1; i <= n; ++i)
@@ -396,7 +562,7 @@ for (int i = 1; i <= n; ++i)
 		s[i][j] = s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1] + a[i][j];
 	}
 }
-求左上角为x1y1右下角为x2y2的矩形的所有数的和
+//求左上角为x1y1右下角为x2y2的矩形的所有数的和
 sum = s[x2][y2] - s[x2][y1 - 1] - s[x1 - 1][y2] + s[x1 - 1][y1 - 1];
 for (int i = 1; i <= n; ++i)
 {
@@ -415,6 +581,7 @@ for (int i = 1; i <= n; ++i)
 }
 sum = s[x2][y2] - s[x2][y1 - 1] - s[x1 - 1][y2] + s[x1 - 1][y1 - 1];
 
+//差分：已知原数组a，构造b数组使得ai=b1+b2...+bi，即原数组是差分数组的前缀和
 //一维差分，原数组是差分数组的前缀和，原数组从a[l]到a[r]每个数加c只要差分数组b[l] = b[l] + c, b[r + 1] = b[r + 1] - c之后求前缀和即可得到所求的a
 //最开时a是已经给出的，b是全0的，可以看成a从全0开始[1, 1]加a[1]，[2, 2]加a[2]，...，[n, n]加a[n]，这样做完后b[i]就有了
 //a是原数组，b是差分数组，b上求前缀和即修改后的a数组
@@ -423,9 +590,11 @@ void insert(int b[], int l, int r, int c)
 	b[l] += c;
 	b[r + 1] -= c;
 }
+for (int i = 1; i <= n; ++i)	insert(b, i, i, a[i]);	//构造好了差分数组
+//insert(b, l, r, c)	//对原数组[l, r]区间中的每个数加上c
 for (int i = 1; i <= n; ++i)
 {
-	b[i] = b[i - 1] + b[i];
+	b[i] = b[i - 1] + b[i];	//又把b数组变成了自己的前缀和，返回原数组
 }
 void insert(int b[], int l, int r, int c)
 {
@@ -447,7 +616,7 @@ for (int i = 1; i <= n; ++i)
 }
 //二维前差分，b[i][j]表示b[1-i][1-j]的所有数的和为a[i][j]，即左上矩阵和等于前缀和，它和一维差分类似
 //a是原数组，b是差分数组，b上求前缀和即修改后的a数组
-void intert(int** b, int x1, int y1, int x2, int y2, int c)
+void insert(int** b, int x1, int y1, int x2, int y2, int c)
 {
 	b[x1][y1] += c;
 	b[x1][y2 + 1] -= c;
@@ -458,7 +627,15 @@ for (int i = 1; i <= n; ++i)
 {
 	for (int j = 1; j <= m; ++j)
 	{
-		b[i][j] = b[i - 1][j] + b[i][j - 1] - b[i - 1][j - 1] + b[i][j];
+		insert(b, i, j, i, j, a[i][j]);	//构造差分数组
+	}
+}
+//insert(b, x1, y1, x2, y2, c);	//原数组一个小矩形加上一个数
+for (int i = 1; i <= n; ++i)
+{
+	for (int j = 1; j <= m; ++j)
+	{
+		b[i][j] = b[i - 1][j] + b[i][j - 1] - b[i - 1][j - 1] + b[i][j];	//差分数组转为前缀和
 	}
 }
 void insert(int** b, int x1, int y1, int x2, int y2, int c)
